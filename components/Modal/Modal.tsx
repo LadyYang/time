fa51dbe546ba140f9bf50dff8360073b90814a3f
ladyyang <1763615252@qq.com>
2020-08-06 14:46:33 +0800

    add:  modal 组件

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,88 @@
+/*
+ * @Description:  Modal 组件
+ * @Author: chtao
+ * @Github: https://github.com/LadyYang
+ * @Email: 1763615252@qq.com
+ * @Date: 2020-08-06 00:24:26
+ * @LastEditTime: 2020-08-06 14:41:30
+ * @LastEditors: chtao
+ * @FilePath: \time\components\Modal\Modal.tsx
+ */
+import React, { useMemo, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from './Modal.module.css';
+import Button from '../Button';
+import { isBrowser } from '../../utils';
+
+export interface ModalProps {
+  visible: boolean;
+  title: string;
+
+  onCancle?: () => void;
+  onOk?: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  children,
+  visible,
+  title,
+  onCancle,
+  onOk,
+}) => {
+  // server-side render
+  if (!isBrowser()) return null;
+
+  const mask = useMemo(() => {
+    const div = document.createElement('div');
+    div.classList.add('mask', styles.container);
+
+    return div;
+  }, []);
+
+  useEffect(() => {
+    visible
+      ? mask.classList.add(styles['show-modal'])
+      : mask.classList.remove(styles['show-modal']);
+  }, [visible]);
+
+  useEffect(() => {
+    document.body.appendChild(mask);
+
+    return () => {
+      document.body.removeChild(mask);
+    };
+  }, [mask]);
+
+  return ReactDOM.createPortal(
+    <div className={styles.modal}>
+      {/* header */}
+      <div className={styles.header}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles['close-btn']} onClick={onCancle}>
+          <i className='iconfont'>&#xe625;</i>
+        </div>
+      </div>
+
+      {/* main content */}
+      <div className={styles.content}>{children}</div>
+
+      {/* footer  */}
+      <div className={styles.footer}>
+        {onCancle && (
+          <Button onClick={onCancle} className={styles.btn}>
+            取消
+          </Button>
+        )}
+        {onOk && (
+          <Button onClick={onOk} className={styles.btn} type='primary'>
+            确认
+          </Button>
+        )}
+      </div>
+    </div>,
+    mask
+  );
+};
+
+export default Modal;
